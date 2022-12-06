e45011b4f9299a3557278ad388b10c091f21df0a
Harsha Karnati <114800403+harshaKARNATI20@users.noreply.github.com>
2022-12-07 00:26:38 +0530

    For Loop Example

---
+++ class63-forEx-CipherSchools.py
@@ -0,0 +1,20 @@
+#sum from 1 to 10
+# 1 + 2 + 3 ......... 10
+
+# total = 0
+# for i in range(1,11):
+#     total += i
+
+# print(total)
+
+# total = 0
+# for i in range(1,21):
+#     total += num
+
+# print(total)
+
+n = int(input("enter the number : "))
+total = 0
+for i in range(1,n+1):
+    total += i
+print(total)
